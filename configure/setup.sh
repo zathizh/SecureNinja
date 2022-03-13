c8a3e293612a7fe397cc64bed8ac023f0474b177
Sathish Bowatta <sathish.bowatta@gmail.com>
2022-03-13 08:08:36 -0400

    Modify setup.sh

--- configure/setup.sh
+++ configure/setup.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
 sudo su <<EOF
+cd /tmp/ && curl -O https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-5.13.tar.gz && tar xzvf man-pages-5.13.tar.gz && cd man-pages-5.13 && make && rm -rf man-pages-*
 echo "kernel.randomize_va_space=0" >  /etc/sysctl.d/01-aslr.conf
 sysctl --system
 reboot
