e1002b51ee8ff99998e20618a85fd4098739396b
Sathish Bowatta <sathish.bowatta@gmail.com>
2022-03-13 08:42:33 -0400

    Modify setup.sh

--- configure/setup.sh
+++ configure/setup.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/bash
 
+# https://www.kernel.org/doc/man-pages/download.html
 manVERSION="man-pages-5.13"
 manURL="https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/$manVERSION.tar.gz"
 
+# Execute as super user.
+# ; install man-pages
+# ; disable aslr
+# ; reboot the system
 sudo su <<EOF
 echo "[+] Downloading $manVERSION"
 cd /tmp/ && curl -O $manURL && tar xzvf "$manVERSION.tar.gz" && cd $manVERSION && make && cd ../ && rm -rf $manVERSION*
