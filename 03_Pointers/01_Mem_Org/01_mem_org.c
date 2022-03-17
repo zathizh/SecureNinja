f4fe0e2d91dc5172b39e2f0a1aa548c397bb9b8f
Sathish H Bowatta <zathizh@yahoo.com>
2022-03-17 10:31:14 +0100

    Update 01_mem_org.c

--- 03_Pointers/01_Mem_Org/01_mem_org.c
+++ 03_Pointers/01_Mem_Org/01_mem_org.c
@@ -1,28 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int GLOBAL_INIT = 1; /* data segment, global */ 
-static int global_uninit; /* BSS segment, global */ 
+#define SHOW_INFO( var ) printf( "    %10p - %10p  %11d %4uB  "#var"\n", &var, (char*)&var + sizeof(var) - 1 , var, sizeof(var) )
 
-int main(int argc, char **argv) { /* stack, local */ 
-	int local_init = 1; /* stack, local */ 
-	int local_uninit; /* stack, local */ 
-	static int local_static_init = 1; /* data seg, local */ 
-	static int local_static_uninit; /* BSS segment, local */ 
-	/* storage for buff_ptr is stack, local */ 
-	/* allocated memory is heap, local */ 
-	int *buff_ptr = (int *)malloc(32); 
+extern char  etext, edata, end; 
+static int GLOBAL_INIT = 1; /* data segment, global */
+static int global_uninit; /* BSS segment, global */
 
-	int *ptr_local_init = &local_init;;
-	int *ptr_local_uninit = &local_uninit;
-	int *ptr_local_static_init = &local_static_init;
-	int *ptr_local_static_uninit = &local_static_uninit;
+int main(int argc, char **argv) { /* stack, local */
+	int local_init = 1; /* stack, local */
+	int local_uninit; /* stack, local */
+	static int local_static_init = 1; /* data segment, local */
+	static int local_static_uninit; /* BSS segment, local */
 
-	printf("local_init : %p\n", ptr_local_init);
-	printf("local_uninit : %p\n", ptr_local_uninit);
-	printf("local_static_init : %p\n", ptr_local_static_init);
-	printf("local_static_uninit : %p\n", ptr_local_static_uninit);
-	printf("buff_ptr [stack] : %p\n", &buff_ptr);
-	printf("buff_ptr memory [heap] : %p\n", buff_ptr);
-	getchar();
+	/* storage for buff_ptr is stack, local */
+	/* allocated memory is heap, local */
+	int *buff_ptr = (int *)malloc(32);
+
+	printf("TEXT SEGMENT %p\n", &etext);
+	printf("\n");
+
+	printf("DATA SEGMENT\n");
+	printf("  static int GLOBAL_INIT = 1; static int local_static_init = 1;\n");
+	SHOW_INFO ( GLOBAL_INIT );
+	SHOW_INFO ( local_static_init );
+
+	printf("\n");
+
+	printf("BSS SEGMENT");
+	printf(" : %p - %p\n", &edata, &end);
+	printf("  static int global_uninit; static int local_static_uninit;\n");
+	SHOW_INFO ( global_uninit );
+	SHOW_INFO ( local_static_uninit );
+
+	printf("\n");
+
+	printf("HEAP\n");
+	printf("  int *buff_ptr = (int *)malloc(32);\n");
+	SHOW_INFO ( *buff_ptr );
+
+	printf("\n");
+
+        printf("STACK\n");
+        printf("  char **argv; int argc; int local_uninit; int local_init=1;\n");
+        SHOW_INFO ( *argv );
+        SHOW_INFO ( argc );
+        SHOW_INFO ( local_uninit );
+        SHOW_INFO ( local_init );
+
+	printf("\n");
+
+	printf("**  Initializing uninitialized variables  **\n");
+	global_uninit = 44;
+	local_uninit = 44;
+	local_static_uninit = 44;
+
+	printf("\n");
+
+	printf("global_uninit = 44; local_uninit = 44; local_static_uninit = 44;\n");
+	SHOW_INFO ( local_uninit );
+	SHOW_INFO ( global_uninit );
+	SHOW_INFO ( local_static_uninit );
+
+	return 0;
 }
