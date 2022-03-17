831bdda7c8f848c08bfe497012a3e7531d203f47
Sathish H Bowatta <zathizh@yahoo.com>
2022-03-17 13:03:04 +0100

    Update 02_vuln_func_ptr.c

--- 03_Pointers/02_Func_Ptr/02_vuln_func_ptr.c
+++ 03_Pointers/02_Func_Ptr/02_vuln_func_ptr.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
-void good_function(const char *str) { 
-	printf("%s", str); 
-} 
+#define SHOW_INFO( var ) printf( "%10p - %10p  %11d %4uB  "#var"\n", &var, (char*)&var + sizeof(var) - 1 , var, sizeof(var) )
+
+#define BUFFSIZE 32
+
+extern char __bss_start, _end;
+
+void good_function(const char *);
  
 int main(void) { 
-static void (*funcPtr)(const char *str); 
-funcPtr = &good_function; 
-(void)(*funcPtr)("hi "); 
-good_function("there!\n"); 
-return 0; 
+	static char buff[BUFFSIZE];
+	static void (*funcPtr)(const char *str); 
+	funcPtr = &good_function; 
+
+	printf("BSS Segment : %p  - %p\n", &__bss_start, &_end);
+	SHOW_INFO ( buff );
+	SHOW_INFO ( funcPtr );
+
+	(void)(*funcPtr)("Hello "); 
+	good_function("World!\n"); 
+
+	return 0; 
+}
+
+void good_function(const char *str) {
+        printf("%s", str);
 }
