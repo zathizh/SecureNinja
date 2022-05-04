85fc669199b22b2be00e77a5a617e426b0effda5
Sathish Bowatta <sathish.bowatta@gmail.com>
2022-05-04 14:02:13 -0400

    add integer promotions

--- 05_Integers/01_Integer_Promotion/02_integer_promotion.c
+++ 05_Integers/01_Integer_Promotion/02_integer_promotion.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(){
-	signed char cresult, c1, c2, c3;
+	signed char cresult, c1, c2;
 	c1 = 100;
 	c2 = 3; 
-	c3 = 4; 
-	cresult = c1 * c2 / c3;
+	cresult = c1 * c2;
 	
-	printf("c1: %d\tc2: %d\tc3: %d\n", c1, c2, c3);
-	printf("%d * %d / %d\n", c1, c2, c3);
+	printf("c1: %d\tc2: %d\n", c1, c2);
+	printf("%d * %d\n", c1, c2);
 	printf("%d\n", cresult);
 	
 	return 0;
